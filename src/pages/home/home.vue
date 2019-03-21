5d73a8b8eaa09e00a94c639127bdab6373d0c230
User65536 <mengxiangji365@163.com>
2019-03-21 17:57:31 +0800

    static homepage

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -40,21 +40,27 @@
         </div>
         <div class="activity-wrap">
           <ul class="activity-list">
-            <li class="activity-item">
-              <div class="item-wrap">
-                <div class="left-img">
-                  <img src="" alt="">
+            <block v-for="(item, index) in activityList" :key="index">
+              <li class="activity-item">
+                <div class="item-wrap">
+                  <div class="left-img">
+                    <img :src="item.img" alt="">
+                  </div>
+                  <div class="right-info">
+                    <div class="title-wrap">
+                      <p class="title">{{item.title}}</p>
+                      <p class="vice-title">{{item.viceTitle}}</p>
+                    </div>
+                    <div class="else-wrap">
+                      <p class="location el-info">{{item.location}}</p>
+                      <p class="time el-info">{{item.time}}</p>
+                      <p class="host el-info">{{item.host}}</p>
+                    </div>
+                    <button class="join" @click="add">报名</button>
+                  </div>
                 </div>
-                <div class="right-info">
-                  <p class="title"></p>
-                  <p class="vice-title"></p>
-                  <p class="location"></p>
-                  <p class="time"></p>
-                  <p class="host"></p>
-                  <button @click="add">立即报名</button>
-                </div>
-              </div>
-            </li>
+              </li>
+            </block>
           </ul>
         </div>
       </div>
@@ -83,6 +89,35 @@ export default {
           url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553149934565&di=567eed359dcf8551e1c009ad2e4f46d7&imgtype=0&src=http%3A%2F%2F00.minipic.eastday.com%2F20170411%2F20170411212923_f3b93bece00e4178f3bc684240278d9a_5.jpeg',
           to: ''
         }
+      ],
+      activityList: [
+        {
+          id: '0',
+          img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553165620324&di=f9d506fff3f67563309f4e40bbd20984&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2Fb90e7bec54e736d1707aa12f91504fc2d56269af.jpg',
+          title: '青春心向党,建功新时代',
+          viceTitle: '以团之名,集卡有你',
+          location: '沙河小区小广场',
+          time: '3-06 12:00',
+          host: '女生部',
+        },
+        {
+          id: '1',
+          img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553165620324&di=f9d506fff3f67563309f4e40bbd20984&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2Fb90e7bec54e736d1707aa12f91504fc2d56269af.jpg',
+          title: '青春心向党,建功新时代',
+          viceTitle: '以团之名,集卡有你',
+          location: '沙河小区小广场',
+          time: '3-06 12:00',
+          host: '女生部',
+        },
+        {
+          id: '2',
+          img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553165620324&di=f9d506fff3f67563309f4e40bbd20984&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2Fb90e7bec54e736d1707aa12f91504fc2d56269af.jpg',
+          title: '青春心向党,建功新时代',
+          viceTitle: '以团之名,集卡有你',
+          location: '沙河小区小广场',
+          time: '3-06 12:00',
+          host: '女生部',
+        },
       ]
     }
   },
