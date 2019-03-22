1524b2036a9242fa31ae7065695642e9b128b83e
User65536 <mengxiangji365@163.com>
2019-03-22 21:54:13 +0800

    详情页静态页面

---
+++ src/pages/detail/detail.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="wrapper-detail">
+    <div class="ticket-wrap">
+      <div class="slot"></div>
+      <div class="ticket">
+        <div class="ticket-content">
+          <div class="top-wrap">
+            <div class="img">
+              <img src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=200057982,2204060083&fm=26&gp=0.jpg" alt="">
+            </div>
+            <div class="title-wrap">
+              <div class="title">2019北京国际长跑节暨北京半程马拉松志愿者招募</div>
+              <div class="vice-title">半马来啦！丰厚福利，阳光等你</div>
+            </div>
+          </div>
+          <div class="middle-wrap">
+            <div class="line line-f">
+              <div class="item">
+                <div class="name">活动时间</div>
+                <div class="content">2019年4月14日(暂定)</div>
+              </div>
+              <div class="item">
+                <div class="name">活动地点</div>
+                <div class="content">天安门广场</div>
+              </div>
+            </div>
+            <div class="line line-s">
+              <div class="item">
+                <div class="name">报名人数</div>
+                <div class="content">30/100</div>
+              </div>
+              <div class="item">
+                <div class="name">举办方</div>
+                <div class="content">阳光志协</div>
+              </div>
+            </div>
+          </div>
+          <div class="bottom-wrap">
+            一场高规格的专业马拉松赛场上，志愿者的工作复杂繁重。从最开始的路线指引、存包、检录，到赛时的补给、助威，参与应急救治，到赛后的拍照、完赛包发放、清理赛场等，志愿者几乎参与到赛事所有的核心工作当中。北京半马更是如此，不过没有挑战，怎么会有成就感！
+          </div>
+        </div>
+        <div class="ticket-vice">
+          <div class="join">立即报名</div>
+          <!-- <div class="code">
+            <div class="qrcode">
+              <img src="" alt="">
+            </div>
+            <div class="barcode">
+              <img src="" alt="">
+              <div class="number"></div>
+            </div>
+          </div> -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import './detail.styl'
+</style>
