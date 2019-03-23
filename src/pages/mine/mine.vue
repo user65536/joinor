22a816705f4642dcabc2165dea1dd5c24cc1f076
User65536 <mengxiangji365@163.com>
2019-03-23 13:29:56 +0800

    static page of mine

---
+++ src/pages/mine/mine.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="wrapper-mine">
+    <div class="ava-area">
+      <img mode="aspectFill" src="https://www.meansky.cn/picture/ava.jpg" alt="" class="ava-bg">
+      <div class="ava-wrap">
+        <div class="ava">
+          <img mode="" src="https://www.meansky.cn/picture/ava.jpg" alt="" class="ava">
+        </div>
+        <div class="username">Trage Mo</div>
+      </div>
+    </div>
+    <div class="bottom-wrap">
+      <div class="content-wrap">
+        <ul class="menu-list">
+          <li class="menu-item">
+            <div class="menu-name">我的报名</div>
+            <div class="right-icon">
+              <span class="iconfont icon-right1"></span>
+            </div>
+          </li>
+          <li class="menu-item">
+            <div class="menu-name">我是主办方</div>
+            <div class="right-icon">
+              <span class="iconfont icon-right1"></span>
+            </div>
+          </li>
+          <li class="menu-item">
+            <div class="menu-name">联系我们</div>
+            <div class="right-icon">
+              <span class="iconfont icon-right1"></span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="tab">
+      <tab-bar @tabchange="tabchange" :active="index"></tab-bar>
+    </div>
+  </div>
+</template>
+
+<script>
+import tabBar from '@/components/tabbar'
+export default {
+  data() {
+    return {
+      index: 1
+    }
+  },
+  methods: {
+    tabchange(e) {
+      this.index = e
+    }
+  },
+  components: {
+    tabBar
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import './mine.styl';
+</style>
