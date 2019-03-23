45c46b8af7114ffcfb63d8f2d0bf1047e89d2b84
User65536 <mengxiangji365@163.com>
2019-03-23 17:03:40 +0800

    已发布页面

---
+++ src/pages/released/released.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrapper-released">
+
+    <div class="tab">
+      <scan-tab @tabchange="tabchange" :active="0"></scan-tab>
+    </div>
+  </div>
+</template>
+
+<script>
+import scanTab from '@/components/scanTab'
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    tabchange (index) {
+      console.log(index)
+    }
+  },
+  components: {
+    scanTab
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper-released
+  .tab
+    position fixed
+    bottom 0
+</style>
