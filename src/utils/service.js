4ad533dd1b01093dd47f6a04c4662509827fb683
User65536 <mengxiangji365@163.com>
2019-06-01 18:14:26 +0800

    新增接口

--- src/utils/service.js
+++ src/utils/service.js
@@ -6,7 +6,10 @@ const port = 8000
 const api = {
   login: '/login',
   release: '/release',
-  getActionList: '/getActionList'
+  getActionList: '/getActionList',
+  getActionDetail: '/getActionDetail',
+  signUp: '/signUp',
+  getEnrolledAction: '/getEnrolledAction'
 }
 
 for(let [k, v] of Object.entries(api)) {
@@ -50,6 +53,39 @@ export default {
     }).then((res) => {
       if(res.code === 200) {
         return Promise.resolve(res.list)
+      } else {
+        return Promise.reject()
+      }
+    })
+  },
+  getActionDetail (id) {
+    return ajax({
+      url: api.getActionDetail,
+      method: 'GET',
+      data: {
+        id
+      }
+    }).then(({detail, state}) => {
+      return Promise.resolve( {detail, state} )
+    })
+  },
+  signUp (id) {
+    return ajax({
+      url: api.signUp,
+      method: 'POST',
+      data: {
+        id
+      }
+    })
+  },
+  getEnrolledAction() {
+    return ajax({
+      url: api.getEnrolledAction,
+      method: 'GET'
+    }).then(result => {
+      console.log(result)
+      if(result.code == 200) {
+        return Promise.resolve(result.list)
       } else {
         return Promise.reject()
       }
